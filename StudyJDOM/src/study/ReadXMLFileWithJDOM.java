ccbe46b8c505884048daf5d81c4e251be4e82ffc
WangWei <waiwong02@outlook.com>
2015-07-24 17:53:51 +0800

    Submit JDOM Study sample

---
+++ StudyJDOM/src/study/ReadXMLFileWithJDOM.java
@@ -0,0 +1,113 @@
+package study;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Date;
+import java.util.Formatter;
+
+import org.jdom2.Attribute;
+import org.jdom2.Comment;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+import util.windowsOS.WinUtil;
+
+public class ReadXMLFileWithJDOM {
+	
+	public static void main(String[] args) {
+		String strPath = new StringBuffer(System.getProperty("user.dir"))
+				.append(File.separator).append("src").toString();
+		
+		System.out.println(strPath);
+		List<String> lstFile = (new WinUtil()).listFilesForFolderByExt(
+				new File(strPath), ".xml");
+		for (String xmlFilePath : lstFile) {
+			System.out.println(xmlFilePath);
+			
+			SAXBuilder saxBuilder = new SAXBuilder();
+			// String xmlFilePath = "./src/testFile.xml";
+			File xmlFile = new File(xmlFilePath);
+			
+			try {
+				Document document = saxBuilder.build(xmlFile);
+				Element rootElement = document.getRootElement();
+				System.out.println(rootElement.getName());
+				List<Element> lstChildren = rootElement.getChildren();
+				for (Element item : lstChildren) {
+					for (Attribute attr : item.getAttributes()) {
+						if (attr.getName().equalsIgnoreCase("id")) {
+							attr.setValue(attr.getValue() + "_1");
+						}
+						
+						System.out.println(String.format("%s:%s",
+								attr.getName(), attr.getValue()));
+					}
+					
+					for (Element itmChild : item.getChildren()) {
+						System.out.println(String.format("%s:%s -> Text:%s",
+								itmChild.getName(), itmChild.getValue(),
+								itmChild.getTextTrim()));
+					}
+					
+					if (item.getChildren().size() > 0) {
+						Date currentDate = new Date();
+						if (item.getChild("age") == null) {
+							Element age = new Element("age").setText("28");
+							age.setAttribute("addTime",
+									String.format("%tc%n", currentDate));
+							item.addContent(age);
+						}
+						
+						Element eleTime = item.getChild("UpdateTime");
+						
+						if (eleTime == null) {
+							eleTime = new Element("UpdateTime").setText(String
+									.format("%tc", currentDate));
+							item.addContent(eleTime);
+						} else {
+							eleTime.setText(String.format("%tc", currentDate));
+						}
+					}
+				}
+				
+				rootElement.addContent(new Comment("Test for add Comment"));
+				XMLOutputter xmlOutput = new XMLOutputter();
+				// display nice nice
+				xmlOutput.setFormat(Format.getPrettyFormat());
+				xmlOutput.output(document, new FileWriter(xmlFilePath));
+				
+				// List listElement = rootElement.getChildren("employee");
+				// for (int i = 0; i < listElement.size(); i++) {
+				// Element node = (Element) listElement.get(i);
+				// System.out.println("ID : " + node.getAttribute("id"));
+				// System.out.println("First Name : "
+				// + node.getChildText("firstname"));
+				// System.out.println("Last Name : "
+				// + node.getChildText("lastname"));
+				// System.out.println("Email : " + node.getChildText("email"));
+				// System.out.println("Department : "
+				// + node.getChildText("department"));
+				// System.out.println("Salary : " +
+				// node.getChildText("salary"));
+				// System.out.println("Address : " +
+				// node.getChildText("address"));
+				// }
+				
+			} catch (IOException io) {
+				System.out.println(io.getMessage());
+			} catch (JDOMException jdomex) {
+				System.out.println(jdomex.getMessage());
+			}
+		}
+	}
+}
---
+++ StudyJDOM/src/util/windowsOS/WinUtil.java
@@ -0,0 +1,128 @@
+package util.windowsOS;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinUtil {
+	
+	public boolean createDir(String dirName) {
+		boolean rslt = false;
+		File dir = new File(dirName);
+		if (!dir.exists()) {
+			if (dir.mkdir()) {
+				rslt = dir.exists();
+			}
+		}
+		
+		return rslt;
+	}
+	
+	public void ListRootDirectories() {
+		Iterable<Path> dirs = FileSystems.getDefault().getRootDirectories();
+		for (Path name : dirs) {
+			System.out.println(name);
+		}
+	}
+	
+	public String getCurrentUserDesktopPath() {
+		String rslt = "";
+		if (System.getProperty("os.name").toLowerCase().indexOf("win") < 0) {
+			System.err.println("Sorry, Windows only!");
+		} else {
+			File desktopDir = new File(System.getProperty("user.home"),
+					"Desktop");
+			if (desktopDir.exists())
+				rslt = desktopDir.getPath();
+		}
+		return rslt;
+	}
+	
+	public List<String> listFilesForFolderByExt(File folder, String fileExt) {
+		
+		List<String> lstRslt = new ArrayList<String>();
+		GenericExtFilter filter = new GenericExtFilter(fileExt);
+		
+		String[] files = folder.list(filter);
+		for (String file : files) {
+			String temp = new StringBuffer(folder.getAbsolutePath())
+					.append(File.separator).append(file).toString();
+			System.out.println("file : " + temp);
+			lstRslt.add(temp);
+		}
+		
+		File[] dirs = folder.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		});
+		
+		for (File fileEntry : dirs) {
+			if (fileEntry.isDirectory()) {
+				System.out.println("Reading files under the folder "
+						+ fileEntry.getAbsolutePath());
+				lstRslt.addAll(listFilesForFolderByExt(fileEntry, fileExt));
+			} else {
+				if (fileEntry.isFile()) {
+					String tmpName = fileEntry.getName();
+					System.out.println("File= " + folder.getAbsolutePath()
+							+ "\\" + fileEntry.getName());
+					// lstRslt.add(tmpName);
+				}
+			}
+		}
+		
+		return lstRslt;
+	}
+	
+	public List<String> listFilesForFolder(File folder) {
+		
+		File[] files = folder.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.isFile();
+			}
+		});
+		
+		List<String> lstRslt = new ArrayList<String>();
+		// List<File> filesInFolder = Files.walk(Paths.get("/path/to/folder"))
+		// .filter(Files::isRegularFile)
+		// .map(Path::toFile)
+		// .collect(Collectors.toList());
+		String tmpName;
+		for (File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				// System.out.println("Reading files under the folder "+folder.getAbsolutePath());
+				lstRslt.addAll(listFilesForFolder(fileEntry));
+			} else {
+				if (fileEntry.isFile()) {
+					tmpName = fileEntry.getName();
+					System.out.println("File= " + folder.getAbsolutePath()
+							+ "\\" + fileEntry.getName());
+					lstRslt.add(tmpName);
+				}
+			}
+		}
+		
+		return lstRslt;
+	}
+	
+	// inner class, generic extension filter
+	public class GenericExtFilter implements FilenameFilter {
+		
+		private String ext;
+		
+		public GenericExtFilter(String ext) {
+			this.ext = ext;
+		}
+		
+		public boolean accept(File dir, String name) {
+			return name.endsWith(ext);
+		}
+	}
+}
