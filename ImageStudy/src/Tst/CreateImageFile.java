1c90b3b4a25f4056473a52de5f2c698fb57c19f0
wangwei <waiwong02@outlook.com>
2016-09-22 20:01:11 +0800

    Update read word from file.

--- ImageStudy/src/Tst/CreateImageFile.java
+++ ImageStudy/src/Tst/CreateImageFile.java
@@ -7,9 +7,15 @@ import java.awt.font.FontRenderContext;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.imageio.ImageIO;
 
 public class CreateImageFile {
@@ -55,21 +61,34 @@ public class CreateImageFile {
 			System.out.println("Current dir using File:" + strFolder);
 			System.out.println("Working Directory = " + System.getProperty("user.dir"));
 			System.out.println("java.class.path:" + System.getProperty("java.class.path"));
-			String desktopPath=System.getProperty("user.home") + "\\Desktop";
-			System.out.println("Desktop:"+desktopPath);
-			String outputFolder=strFolder + File.separator +"OutputPic";
+			String desktopPath = System.getProperty("user.home") + "\\Desktop";
+			System.out.println("Desktop:" + desktopPath);
+			String outputFolder = strFolder + File.separator + "OutputPic";
 			(new File(outputFolder)).mkdirs();
-			System.out.println("Output folder:" + outputFolder);			
-			String srcStr = "启功字体简繁体";
+			System.out.println("Output folder:" + outputFolder);
+			String wordsFileName = strFolder + File.separator + "Words.txt";
+			System.out.println("wordsFileName folder:" + wordsFileName);
+			List<String> list = new ArrayList<>();
+			try (InputStreamReader isr = new InputStreamReader(new FileInputStream(wordsFileName), "UTF-8");
+					BufferedReader br = new BufferedReader(isr);) {
+				list = br.lines().collect(Collectors.toList());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			list.forEach(System.out::println);
 			int indexName = 0;
-			for (char ch : srcStr.toCharArray()) {
-				String str = String.valueOf(ch);
-				System.out.println(str);
-				cg.graphicsGeneration(str, strFolder + File.separator + "启功字体繁体.ttf",
-						outputFolder + File.separator + String.valueOf(indexName) + "0.jpg");
-				cg.graphicsGeneration(str, strFolder + File.separator + "启功字体简体.TTF",
-						outputFolder + File.separator + String.valueOf(indexName) + "1.jpg");
-				indexName++;
+			for (String strLine : list) {
+				System.out.println(strLine);
+				for (char ch : strLine.toCharArray()) {
+					String str = String.valueOf(ch);
+					System.out.println(str);
+					cg.graphicsGeneration(str, strFolder + File.separator + "启功字体繁体.ttf",
+							outputFolder + File.separator + String.valueOf(indexName) + "0.jpg");
+					cg.graphicsGeneration(str, strFolder + File.separator + "启功字体简体.TTF",
+							outputFolder + File.separator + String.valueOf(indexName) + "1.jpg");
+					indexName++;
+				}
 			}
 
 			System.out.println("Generate OK.");
